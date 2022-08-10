ff1523d854e040bfb2b4682bd4063c34f7caf8cc
cdchinmoy <cdchinmoy@gmail.com>
2022-08-10 17:42:17 +0530

    first commit

--- api/serializer.py
+++
@@ -1,6 +0,0 @@
-from rest_framework import serializers
-
-class FileUploadSerializer(serializers.Serializer):
-    file = serializers.FileField()
-    Converted_Currency = serializers.CharField()
-    
--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-from .views import (CurrencyConverterApiView)
-
-urlpatterns = [
-    path('currency/convert', CurrencyConverterApiView.as_view()),
-]
